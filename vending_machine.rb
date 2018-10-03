24ffb5b82672dcc8ffe6a26cfb56ee338d09f638
kenichi sonoda <sonoda@val.co.jp>
2018-10-03 21:58:46 +0900

    リファクタリング（vend関数とreceiveMoney関数に切り分け）

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,33 +1,33 @@
 PRICE_COKE = 100
 
 # 金をうけつける細胞
+# お金以外の物は確実に弾く
+# お金でも[10, 50, 100, 500, 1000]以外は弾く
 def receiveMoney object
+  usable_money = [10, 50, 100, 500, 1000]
   # お金以外のものがきたら弾くよ
+  if usable_money.include?(money)
+  end
+  if is_money? money
+    if money == 0
+      puts "お金は入れようね"
+    else
+      puts "#{money}円は返すわ。"
+    end
+  else
+    puts "どら焼きは返すわ。"
+  end
 end
 
 # insertで弾いてくれてるので確実に引数はお金です
 # 飲み物排出細胞
-def buy money
-  usable_money = [10, 50, 100, 500, 1000]
-  if usable_money.include?(money)
-    if money >= PRICE_COKE
-      change = money - PRICE_COKE
-      puts money.to_s + "円でコカ・コーラ"
-      puts "おつりは#{change}円ですよ" if change > 0
-    else
-      puts "足りねーよ！"
-    end
+def vend money
+  if money >= PRICE_COKE
+    change = money - PRICE_COKE
+    puts "コカ・コーラ（#{PRICE_COKE}円）を買いました。"
+    puts "おつりは#{change}円です。" if change > 0
   else
-    print "買えねーよ！"
-    if is_money? money
-      if money == 0
-        puts "お金は入れようね"
-      else
-        puts "#{money}円は返すわ。"
-      end
-    else
-      puts "どら焼きは返すわ。"
-    end
+    puts "足りねーよ！#{money}円は返すわ。"
   end
 end
 
@@ -35,20 +35,20 @@ def is_money? object
   Integer === object
 end
 
-# buyが受け付けるテスト例
-buy 10
-buy 50
-buy 100
-buy 500
-buy 1000
+# vendが受け付けるテスト例
+vend 10
+vend 50
+vend 100
+vend 500
+vend 1000
 # receiveMoneyが受け付けるテスト例
-buy 0
-buy 1
-buy 5
-buy 2000
-buy 5000
-buy 3.14
-buy 99
-buy 120
-buy "どら焼き"
-buy "100"
+# receiveMoney 0
+# receiveMoney 1
+# receiveMoney 5
+# receiveMoney 2000
+# receiveMoney 5000
+# receiveMoney 3.14
+# receiveMoney 99
+# receiveMoney 120
+# receiveMoney "どら焼き"
+# receiveMoney "100"
