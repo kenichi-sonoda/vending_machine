4f1677f632b57da45f05680bf7a63694d1451ebe
kenichi sonoda <sonoda@val.co.jp>
2018-10-03 20:42:33 +0900

    扱えるお金と買える金額を指定できるようになりました

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,14 +1,22 @@
 def buy money
-  usable_money = [100, 120]
+  usable_money = [10, 50, 100, 500, 1000]
   if usable_money.include?(money)
-    puts money.to_s + "円でコカ・コーラ"
+    if money >= 100
+      puts money.to_s + "円でコカ・コーラ"
+    else
+      puts "足りねーよ！"
+    end
   else
     puts "買えねーよ！"
   end
 end
 
-buy 99
+buy 10
+buy 50
 buy 100
+buy 500
+buy 1000
+buy 99
 buy 120
 buy "どら焼き"
 buy "100"
