0fd9632c700689b16c5727f2391b9e7d30b53510
kenichi sonoda <sonoda@val.co.jp>
2018-10-03 20:32:13 +0900

    買える金額を絞りました

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,11 +1,13 @@
 def buy money
-  if money == 100
+  usable_money = [100, 120]
+  if usable_money.include?(money)
     puts money.to_s + "円でコカ・コーラ"
   else
     puts "買えねーよ！"
   end
 end
 
+buy 99
 buy 100
 buy 120
 buy "どら焼き"
