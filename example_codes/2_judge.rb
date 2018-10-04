29090926712eec1e6847b97c053a6ce08791f5a9
kenichi sonoda <sonoda@val.co.jp>
2018-10-04 11:42:18 +0900

    適当すぎたので編集

--- example_codes/2_judge.rb
+++ example_codes/2_judge.rb
@@ -1,11 +1,18 @@
+price = 150
+
 # rubyでif elseは以下のとおりです
-if true
+if price >= 100
   puts "これが表示されます"
-elsif true
-  puts "表示されません"
+elsif price >= 50
+  puts "もちろん表示されません"
 else
   puts "ちなみにif文の（）も要りません"
 end
 
 # 一行でまとめることもできます
-puts "後置if" if true
+# ただし条件が長くなりすぎると可読性悪いので注意です
+puts "これは後置ifと言われます。" if price >= 100
+
+# 三項演算子も使えます
+# 条件 : trueの場合の処理 ? falseの場合の処理
+price >= 100 : "前にtrueの場合の結果" ? "後ろにfalseの場合の結果"
