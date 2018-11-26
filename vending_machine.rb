c9b9142a4c77aeed532174721e7ede2938524d85
kenichi sonoda <sonoda@val.co.jp>
2018-11-26 21:35:03 +0900

    160円ぴったりでジンジャーエールが買えるようになりました

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,11 +1,15 @@
 # Let's enjoy (Mob) programming!!
+puts 'コーラ: 100円, ジンジャーエール :160円'
+puts '※ジンジャーエールは　160円ぴったり入れてください'
 puts 'お金を入れてください'
 money = gets.to_i
 puts "投入金額 #{money} 円"
 if money == 100
-  puts 'coke'
+  puts 'コーラ'
+elsif money ==160
+  puts 'ジンジャーエール'
 elsif money > 100
-  puts 'coke'
+  puts 'コーラ'
   puts "お釣り #{money - 100} 円"
 else
   puts 'お金が足りませんでした'
