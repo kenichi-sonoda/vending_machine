064f675d5970b228a14132770f7c3a1ae355ad2b
kenichi sonoda <sonoda@val.co.jp>
2018-11-26 21:26:05 +0900

    自動販売機に近づけた

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,12 +1,12 @@
 # Let's enjoy (Mob) programming!!
 puts 'お金を入れてください'
-money=gets.to_i
-puts money
+money = gets.to_i
+puts "投入金額 #{money} 円"
 if money == 100
   puts 'coke'
 elsif money > 100
   puts 'coke'
-  puts money - 100
+  puts "お釣り #{money - 100} 円"
 else
   puts 'お金が足りませんでした'
 end
