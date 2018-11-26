57b877224034a3640a6d4f55ea2c6fb48c0a6d54
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-11-26 21:37:34 +0900

    add okane wo ireru syori

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,10 +1,12 @@
 # Let's enjoy (Mob) programming!!
 @juice_list = {cola: {price: 100, stock: 0}, milk: {price: 100, stock: 3}, water: {price: 100, stock: 3}}
 
-def buy(juice_name)
-  if @juice_list.has_key?(juice_name) && @juice_list[juice_name][:stock] > 0
+def buy(juice_name, money)
+  if @juice_list.has_key?(juice_name) && @juice_list[juice_name][:stock] > 0 && money >= @juice_list[juice_name][:price]
     puts juice_name
     @juice_list[juice_name][:stock] -= 1
+  elsif money < @juice_list[juice_name][:price]
+    puts 'お金が足りません'
   elsif !@juice_list.has_key?(juice_name)
     puts '取り扱いがございません'
   else
@@ -17,7 +19,11 @@ def show_juice_list
 end
 
 show_juice_list
+puts "お金を入れてください"
+money = gets.chomp.to_i
+
 puts "何を買いますか？"
 
 juice = gets.chomp.to_sym
-buy(juice)
+
+buy(juice, money)
