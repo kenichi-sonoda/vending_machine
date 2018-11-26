55e2e46b471930280e7f1bb2d2893dacfd91b535
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-11-26 21:32:31 +0900

    add function available to buy juice

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,8 +1,23 @@
 # Let's enjoy (Mob) programming!!
-JUICE_LIST = %w[cola milk water tea]
+@juice_list = {cola: {price: 100, stock: 0}, milk: {price: 100, stock: 3}, water: {price: 100, stock: 3}}
 
-def buy(juice_list,num)
-  puts juice_list[num]
+def buy(juice_name)
+  if @juice_list.has_key?(juice_name) && @juice_list[juice_name][:stock] > 0
+    puts juice_name
+    @juice_list[juice_name][:stock] -= 1
+  elsif !@juice_list.has_key?(juice_name)
+    puts '取り扱いがございません'
+  else
+    puts '売り切れです'
+  end
 end
 
-buy(JUICE_LIST,1)
+def show_juice_list
+  puts  @juice_list
+end
+
+show_juice_list
+puts "何を買いますか？"
+
+juice = gets.chomp.to_sym
+buy(juice)
