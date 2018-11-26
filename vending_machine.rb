920e32fca1f6481fbb2bbf578e950999921db1b6
kenichi sonoda <sonoda@val.co.jp>
2018-11-26 21:13:10 +0900

    お釣りが出るようになりました

--- vending_machine.rb
+++ vending_machine.rb
@@ -2,8 +2,11 @@
 puts 'お金を入れてください'
 money=gets.to_i
 puts money
-if money >= 100
+if money == 100
   puts 'coke'
+elsif money > 100
+  puts 'coke'
+  puts money - 100
 else
   puts 'お金が足りませんでした'
 end
